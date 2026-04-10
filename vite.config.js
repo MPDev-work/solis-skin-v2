2fa81f36681035a217d3b395de24e32ea4e95722
MPDev <pisalmony.work@gmail.com>
2026-04-10 13:27:06 +0700

    Fix assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './',
+  base: '/solis-skin-v2',
   plugins: [react(), tailwindcss()],
 });
