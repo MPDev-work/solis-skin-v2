1fe92541ae5325e66aa4d5279e4163e67c56994b
MPDev <pisalmony.work@gmail.com>
2026-04-10 14:06:57 +0700

    Update UI

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/solis-skin-v2',
   plugins: [react(), tailwindcss()],
+  base: '/lumi-petal/',
 });
