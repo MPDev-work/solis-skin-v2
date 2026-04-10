96d268ef26dd65a29a47d21cb158d5edd67c1576
MPDev <pisalmony.work@gmail.com>
2026-04-10 13:21:47 +0700

    Fix UI

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: './solis-skin',
+  base: './',
   plugins: [react(), tailwindcss()],
 });
